bbcb1b27de6080223c281cd83453cdbe92420da6
Takudzwa Gwindingwi <takueg@gmail.com>
2021-07-10 16:07:30 +0200

    adding delete feature

--- config/routes.rb
+++ config/routes.rb
@@ -15,4 +15,8 @@ Rails.application.routes.draw do
   get "tasks/:id/edit", to: "tasks#edit", as: :edit
   patch "tasks/:id", to: "tasks#update"
 
+  # destroy a task
+  delete "/tasks/:id", to: "tasks#destroy"
+
+
 end
